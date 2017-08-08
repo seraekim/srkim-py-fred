54965df4bb34cd737d0005e08524d9a3042d1f57
Kim Serae <kserae@gmail.com>
2017-08-09 01:11:35 +0900

    update

--- 04_update.py
+++ 04_update.py
@@ -12,6 +12,7 @@ init이 완료되면 알아서 가장 오래된 파일의 mtime을 체크하고 
 conn = HTTPSConnection(fred_domain)
 
 cnt = 0
+method_cnt = 0
 is_end = False
 
 # api 상 1000이 기본값, 바꾸게되면 limit, offset도 동일하게 적용해야 데이터 안 꼬인다.
@@ -29,11 +30,15 @@ print(last_start_time)
 
 
 def get_update_series(series_list):
-    print('[method] get_update_series start!!!')
     global cnt
+    global method_cnt
     global update_limit
     global update_offset
     global is_end
+
+    method_cnt += 1
+
+    print('[method] get_update_series start count :', method_cnt)
 
     for series_item in series_list:
         cnt += 1
@@ -42,12 +47,15 @@ def get_update_series(series_list):
 
         # last_update_start_time.txt 보다 last_updated 가 이전이 되면 더 이상 업데이트는 없는 걸로 판단
         is_end = update_time < last_start_time
-        print(is_end, update_time, last_start_time)
+        print(cnt, update_time, update_id)
         if is_end:
             # 업데이트 '시작'을 기록하고 종료.. 업데이트 '종료' 시간이 아닌 이유는, 기록 되는 동안에도 시간이
             # 흘러가기 때문에,..
-            with open(fred_ids_file_last_update_time, 'w', encoding='utf-8') as f:
-                f.write(this_start_time)
+            # 다만 테스트로 돌린 경우라면 마지막 업데이트 시간을 갱신하지 않을 것.
+            if chunk_unit == 1000:
+                with open(fred_ids_file_last_update_time, 'w', encoding='utf-8') as f:
+                    f.write(this_start_time)
+
             break
         else:
             ret = req(conn, fred_series_observ_url + update_id)
